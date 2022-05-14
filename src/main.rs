5370d067ee90c9069835c10ef07d3564bbd5d288
Kreedzt <zhaozisong1@live.com>
2022-05-14 13:07:17 +0800

    :lipstick: (layout) add group layout

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@
 
 use qt_widgets::cpp_core::{NullPtr, Ptr, Ref, StaticUpcast};
 use qt_widgets::qt_core::{qs, QTime, QTimer, SlotNoArgs, TextFormat};
-use qt_widgets::qt_gui::QTextFormat;
+use qt_widgets::qt_gui::{QTextFormat, QGradient};
 use qt_widgets::{
-    QApplication, QLabel, QLineEdit, QMessageBox, QPushButton, QToolBar, QVBoxLayout, QWidget,
+    QApplication, QLabel, QLineEdit, QMessageBox, QPushButton, QToolBar, QVBoxLayout, QWidget, QGroupBox,
 };
 
 use std::cell::RefCell;
@@ -17,26 +17,60 @@ use constant::APPLICATION_NAME;
 
 fn main() {
     QApplication::init(|app| unsafe {
+        // 全局
         let widget = QWidget::new_0a();
         let layout = QVBoxLayout::new_1a(&widget);
         widget.set_window_title(&qs(APPLICATION_NAME));
         widget.set_minimum_width(400);
 
-        // let toolbar = QToolBar::new();
+        // weapons 分组
+        let weapon_group_box = QGroupBox::new();
+        weapon_group_box.set_title(&qs("weapons"));
+        let weapon_group_layout = QVBoxLayout::new_0a();
 
-        let label = QLabel::new();
-        label.set_text(&qs("输入Mod文件夹路径(通常以 2513537759 为末尾)"));
-        label.set_text_format(TextFormat::PlainText);
-        layout.add_widget(&label);
+        let weapons_label = QLabel::new();
+        weapons_label.set_text(&qs("输入weapons文件夹路径(提取武器文件需要)"));
+        weapons_label.set_text_format(TextFormat::PlainText);
+        weapon_group_layout.add_widget(&weapons_label);
 
-        let line_edit = QLineEdit::new();
-        layout.add_widget(&line_edit);
+        let weapons_line_edit = QLineEdit::new();
+        weapon_group_layout.add_widget(&weapons_line_edit);
+
+        let weapons_translation_label = QLabel::new();
+        weapons_translation_label.set_text(&qs("输入weapons翻译文件路径(提取武器文件需要, 非必须)"));
+        weapons_translation_label.set_text_format(TextFormat::PlainText);
+        weapon_group_layout.add_widget(&weapons_translation_label);
+
+        let weapons_translation_line_edit = QLineEdit::new();
+        weapon_group_layout.add_widget(&weapons_translation_line_edit);
 
         let button = QPushButton::from_q_string(&qs("开始提取武器数据(.csv文件)"));
-        layout.add_widget(&button);
+        weapon_group_layout.add_widget(&button);
+
+        // weapons 布局添加完毕, 追加到总布局
+        weapon_group_box.set_layout(&weapon_group_layout);
+        layout.add_widget(&weapon_group_box);
+
+        // armor 分组
+        let armor_group_box = QGroupBox::new();
+        armor_group_box.set_title(&qs("armor"));
+
+        let armor_group_layout = QVBoxLayout::new_0a();
+
+        let armor_label = QLabel::new();
+        armor_label.set_text(&qs("输入items文件夹路径(提取护甲数据需要)"));
+        armor_label.set_text_format(TextFormat::PlainText);
+        armor_group_layout.add_widget(&armor_label);
+
+        let armor_line_edit = QLineEdit::new();
+        armor_group_layout.add_widget(&armor_line_edit);
 
         let button2 = QPushButton::from_q_string(&qs("开始提取护甲数据(.csv文件)"));
-        layout.add_widget(&button2);
+        armor_group_layout.add_widget(&button2);
+
+        // armor 布局添加完毕, 追加到总布局
+        armor_group_box.set_layout(&armor_group_layout);
+        layout.add_widget(&armor_group_box);
 
         let label = QLabel::new();
         label.set_text(&qs(format!(
@@ -55,7 +89,7 @@ fn main() {
         widget.show();
 
         let buton_clicked = SlotNoArgs::new(NullPtr, move || {
-            let text = line_edit.text();
+            let text = armor_line_edit.text();
             // QMessageBox::information_q_widget2_q_string(
             //     &widget,
             //     &qs("Example"),
