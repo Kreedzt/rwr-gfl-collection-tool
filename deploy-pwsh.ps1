e1a5c72681685ad7a75e10d30b041b4730aedd50
Kreedzt <zhaozisong1@live.com>
2022-05-14 18:30:20 +0800

    :bug: (fixed ps1 error)

--- deploy-pwsh.ps1
+++ deploy-pwsh.ps1
@@ -1,4 +1,4 @@
 cp .\target\release\rwr-gfl-mod-collection-tool.exe deploy\
-cp .\LICESE deploy\
+cp .\LICENSE deploy\
 windeployqt.exe .\deploy\rwr-gfl-mod-collection-tool.exe --dir deploy --release --no-translations --no-angle --no-opengl-sw --no-quick-import --no-virtualkeyboard --no-compiler-runtime --no-webkit2
 7z.exe a deploy.7z .\deploy\*
